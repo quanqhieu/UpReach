03ca1c61354606176ee601e634db3ff359f84e6b
ChienNguyen <chiennguyen18102001c@gmail.com>
2023-07-04 11:58:00 +0700

    fix_code

--- fe_capstone_upreach/src/Components/InfluProfileModal/InfluProfile.jsx
+++ fe_capstone_upreach/src/Components/InfluProfileModal/InfluProfile.jsx
@@ -3,30 +3,6 @@ import InfluSideBar from "./InfluSideBar/InfluSideBar";
 import InfluImage from "./InfluImage/InfluImage";
 import React from "react";
 const InfluProfile = ({ profileInflu }) => {
-  // const [influInfo, setInfluInfo] = React.useState({
-  //   fullName: "Le Quang Hieu",
-  //   email: "hieupro123@gmail.com",
-  //   age: 22,
-  //   phone: "0398357123",
-  //   gender: "Male",
-  //   bio: "Sang som thuc day bong thay minh qua dep trai, tieng hot thanh thot cua nhung chu chim vua sang, ngay vui lai len...",
-  //   address: "Da Nang",
-  //   relationship: "Married",
-  //   topics: [
-  //     "Baby",
-  //     "Beauty",
-  //     "Business",
-  //     "Travel",
-  //     "Game",
-  //     "Film",
-  //     "Personal perception",
-  //     "Film",
-  //     "Film",
-  //     "Film",
-  //   ],
-  //   type: "Citizen",
-  // });
-
   return (
     <>
       <div className="influ-profile">
