90eb1277cf8a07812963e3c8578a5ae06313b44d
DESKTOP-J55FVB8\PC <dattruong12012001@gmail.com>
2023-08-26 17:09:50 +0700

    Update code

--- fe_capstone_upreach/src/Pages/ForgotPassword/VerifyForgotPassword.jsx
+++ fe_capstone_upreach/src/Pages/ForgotPassword/VerifyForgotPassword.jsx
@@ -2,24 +2,23 @@ import React, { useEffect, useRef, useState } from "react";
 import { Button, Form, Input, InputNumber } from "antd";
 import { useNavigate } from "react-router-dom";
 // import "./VerifyRegisterPage.css";
-import "../VerifyRegister/VerifyRegisterPage.css"
-import ApiUser from "../../Api/ApiUser"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "../VerifyRegister/VerifyRegisterPage.css";
+import ApiUser from "../../Api/ApiUser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const VerifyForgotPassword = () => {
-
   const navigate = useNavigate();
-  const [message, setMessage] = useState()
-  const [otp, setOtp] = useState(new Array(6).fill(""))
+  const [message, setMessage] = useState();
+  const [otp, setOtp] = useState(new Array(6).fill(""));
   const inputRefs = useRef([]);
-  const [status, setStatus] = useState()
-  const [formForgotPass,setFormForgotPass ] = useState({});
+  const [status, setStatus] = useState();
+  const [formForgotPass, setFormForgotPass] = useState({});
 
-  useEffect(()=>{
-    if(status === 'True'){
-      navigate('/reset-password')
+  useEffect(() => {
+    if (status === "True") {
+      navigate("/reset-password");
     }
-  },[status])
+  }, [status]);
 
   const toastOptions = {
     position: "bottom-right",
@@ -27,37 +26,41 @@ const VerifyForgotPassword = () => {
     pauseOnFocusLoss: true,
     draggable: true,
     theme: "dark",
-  }
+  };
 
   const confirmUserForgotPass = async (data) => {
     try {
       const response = await ApiUser.confirmForgotPassword(data);
-      console.log(response)
+      console.log(response);
       if (response.status === "False") {
-        toast.error(response.message, toastOptions)
-        setStatus(response.status)
+        toast.error(response.message, toastOptions);
+        setStatus(response.status);
         return false;
       }
-      toast.success(response.message, toastOptions)
-      setStatus(response.status)
-      return response
+      toast.success(response.message, toastOptions);
+      setStatus(response.status);
+      return response;
     } catch (error) {
-      setMessage(error)
+      setMessage(error);
       throw error;
     }
-  }
+  };
 
   const handleInputChange = (element, index) => {
-    console.log(element)
+    console.log(element);
     if (isNaN(element)) {
       return false;
-    };
+    }
 
-    setOtp([...otp.map((d, idx) => (idx === index) ? element : d)])
+    setOtp([...otp.map((d, idx) => (idx === index ? element : d))]);
 
     if (element === null && index > 0) {
       inputRefs.current[index - 1].focus(); // Move focus to the previous input
-    } else if (element !== null && element.toString().length === 1 && index < otp.length - 1) {
+    } else if (
+      element !== null &&
+      element.toString().length === 1 &&
+      index < otp.length - 1
+    ) {
       inputRefs.current[index + 1].focus(); // Move focus to the next input
     }
   };
@@ -68,61 +71,70 @@ const VerifyForgotPassword = () => {
     }
   };
 
-
   const handleSubmit = async () => {
     const dataOtp = otp.join("");
-    formForgotPass.otp = '' + dataOtp
-    confirmUserForgotPass(formForgotPass)
-  }
+    formForgotPass.otp = "" + dataOtp;
+    confirmUserForgotPass(formForgotPass);
+  };
 
   return (
     <>
-      <div className="verify-background">
-        <div className="verify-custom-bg">
-          <div className="form-holder reset-form" style={{ display: "block" }}>
-            <h2 className="form-title">Verify your email </h2>
-            <div className="sub-title">
-              <p>
-                We sent an email to your email, Please check your inbox and
-                enter the 6-digit code to verify your email.
-              </p>
+      <div className="auth-background">
+        <div className="verify-background">
+          <div className="verify-custom-bg">
+            <div className="auth-custom-line1 index-100"></div>
+            <div className="auth-custom-line2 index-100"></div>
+            <div className="auth-custom-line3 index-100"></div>
+            <div className="auth-custom-line5 index-100">
+              <div className="auth-custom-line4 index-100"></div>
             </div>
-            <Form className="form" onFinish={handleSubmit}>
-              <Form.Item
-                name="name"
-                className="form-otp"
-              >
+            <div
+              className="form-holder reset-form index-1000"
+              style={{ display: "block" }}
+            >
+              <h2 className="form-title">Verify your email </h2>
+              <div className="sub-title">
+                <p>
+                  We sent an email to your email, Please check your inbox and
+                  enter the 6-digit code to verify your email.
+                </p>
+              </div>
+              <Form className="form" onFinish={handleSubmit}>
+                <Form.Item name="name" className="form-otp">
+                  {otp.map((data, index) => {
+                    return (
+                      <InputNumber
+                        className="otp-filed"
+                        maxLength={1}
+                        min={0}
+                        key={index}
+                        defaultValue={null}
+                        value={data}
+                        onChange={(e) => handleInputChange(e, index)}
+                        onFocus={() => handleInputFocus(index)}
+                        ref={(ref) => (inputRefs.current[index] = ref)}
+                      />
+                    );
+                  })}
+                </Form.Item>
+                <Button
+                  style={{
+                    width: "500px",
+                    height: "50px",
+                    margin: "0 auto",
+                    backgroundColor: "#000",
+                    color: "white",
+                  }}
+                  className="submit btn"
+                  htmlType="submit"
+                >
+                  Continue
+                </Button>
 
-                {otp.map((data, index) => {
-                  return (
-                    <InputNumber
-                      className="otp-filed"
-                      maxLength={1}
-                      min={0}
-                      key={index}
-                      defaultValue={null}
-                      value={data}
-                      onChange={(e) => handleInputChange(e, index)}
-                      onFocus={() => handleInputFocus(index)}
-                      ref={(ref) => (inputRefs.current[index] = ref)}
-                    />
-                  )
-                })}
-              </Form.Item>
-              <Button
-                style={{
-                  width: "500px",
-                  height: "50px",
-                }}
-                className="submit btn"
-                htmlType="submit"
-              >
-                Continue
-              </Button>
-
-              <div className="login-forgot">I didn't receive an email</div>
-              <ToastContainer />
-            </Form>
+                {/* <div className="login-forgot">I didn't receive an email</div> */}
+                <ToastContainer />
+              </Form>
+            </div>
           </div>
         </div>
       </div>
--- fe_capstone_upreach/src/Pages/VerifyRegister/VerifyRegisterPage.jsx
+++ fe_capstone_upreach/src/Pages/VerifyRegister/VerifyRegisterPage.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useRef, useState } from "react";
 import { Button, Form, Input, InputNumber } from "antd";
 import { useNavigate } from "react-router-dom";
 import "./VerifyRegisterPage.css";
-import ApiUser from "../../Api/ApiUser"
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import ApiUser from "../../Api/ApiUser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AuthBackground from "../../Components/Layouts/AuthBackground/AuthBackground";
+import "../../Components/Layouts/AuthBackground/AuthBackground.css";
 const VerifyRegisterPage = () => {
-
   const navigate = useNavigate();
-  const [message, setMessage] = useState()
-  const [otp, setOtp] = useState(new Array(6).fill(""))
+  const [message, setMessage] = useState();
+  const [otp, setOtp] = useState(new Array(6).fill(""));
   const inputRefs = useRef([]);
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPass: ''
+    name: "",
+    email: "",
+    password: "",
+    confirmPass: "",
   });
 
   const toastOptions = {
@@ -24,46 +25,49 @@ const VerifyRegisterPage = () => {
     pauseOnFocusLoss: true,
     draggable: true,
     theme: "dark",
-  }
+  };
 
   useEffect(() => {
-    const data = localStorage.getItem('formData');
+    const data = localStorage.getItem("formData");
     if (!data) {
-      navigate("/sign-up")
+      navigate("/sign-up");
     }
     const formDataJson = JSON.parse(data);
-    setFormData(formDataJson)
-
+    setFormData(formDataJson);
   }, [navigate]);
 
   const confirmUser = async (data) => {
     try {
       const response = await ApiUser.conFirmUser(data);
-      console.log(response)
+      console.log(response);
 
       if (response.status === "False") {
-        toast.error(response.message, toastOptions)
+        toast.error(response.message, toastOptions);
         return false;
       }
-      toast.success(response.message, toastOptions)
-      return response
+      toast.success(response.message, toastOptions);
+      return response;
     } catch (error) {
-      setMessage(error)
+      setMessage(error);
       throw error;
     }
-  }
+  };
 
   const handleInputChange = (element, index) => {
-    console.log(element)
+    console.log(element);
     if (isNaN(element)) {
       return false;
-    };
+    }
 
-    setOtp([...otp.map((d, idx) => (idx === index) ? element : d)])
+    setOtp([...otp.map((d, idx) => (idx === index ? element : d))]);
 
     if (element === null && index > 0) {
       inputRefs.current[index - 1].focus(); // Move focus to the previous input
-    } else if (element !== null && element.toString().length === 1 && index < otp.length - 1) {
+    } else if (
+      element !== null &&
+      element.toString().length === 1 &&
+      index < otp.length - 1
+    ) {
       inputRefs.current[index + 1].focus(); // Move focus to the next input
     }
   };
@@ -74,73 +78,83 @@ const VerifyRegisterPage = () => {
     }
   };
 
-
   const handleSubmit = async () => {
     const dataOtp = otp.join("");
-    formData.otp = '' + dataOtp
-    const result = await confirmUser(formData)
+    formData.otp = "" + dataOtp;
+    const result = await confirmUser(formData);
     if (result) {
       if (message) {
-        navigate('/verify-register')
+        navigate("/verify-register");
       }
-      if (formData.role === '2') {
-        navigate('/client-profile')
-      }
-      else if (formData.role === '3') {
-        navigate('/create-influencer-page')
+      if (formData.role === "2") {
+        navigate("/client-profile");
+      } else if (formData.role === "3") {
+        navigate("/create-influencer-page");
       }
     }
-
-  }
+  };
 
   return (
     <>
-      <div className="verify-background">
-        <div className="verify-custom-bg">
-          <div className="form-holder reset-form" style={{ display: "block" }}>
-            <h2 className="form-title">Verify your email </h2>
-            <div className="sub-title">
-              <p>
-                We sent an email to {formData.email} Check your inbox and
-                enter the 6-digit code to verify your email.
-              </p>
+      <div className="auth-background">
+        <div className=" verify-background">
+          <div className="verify-custom-bg">
+            <div className="auth-custom-line1 index-100"></div>
+            <div className="auth-custom-line2 index-100"></div>
+            <div className="auth-custom-line3 index-100"></div>
+            <div className="auth-custom-line5 index-100">
+              <div className="auth-custom-line4 index-100"></div>
             </div>
-            <Form className="form" onFinish={handleSubmit}>
-              <Form.Item
-                name="name"
-                className="form-otp"
-              >
+            <div
+              className="form-holder reset-form index-1000"
+              style={{ display: "block" }}
+            >
+              <h2 className="form-title text-center index-1000">
+                Verify your email{" "}
+              </h2>
+              <div className="sub-title">
+                <p>
+                  We sent an email to {formData.email} Check your inbox and
+                  enter the 6-digit code to verify your email.
+                </p>
+              </div>
+              <Form className="form" onFinish={handleSubmit}>
+                <Form.Item name="name" className="form-otp">
+                  {otp.map((data, index) => {
+                    return (
+                      <InputNumber
+                        className="otp-filed"
+                        maxLength={1}
+                        min={0}
+                        key={index}
+                        defaultValue={null}
+                        value={data}
+                        onChange={(e) => handleInputChange(e, index)}
+                        onFocus={() => handleInputFocus(index)}
+                        ref={(ref) => (inputRefs.current[index] = ref)}
+                      />
+                    );
+                  })}
+                </Form.Item>
+                <Button
+                  style={{
+                    width: "500px",
+                    height: "50px",
+                    margin: "0 auto",
+                    backgroundColor: "#000",
+                    color: "white",
+                  }}
+                  className="submit btn"
+                  htmlType="submit"
+                  type="primary"
+                >
+                  Continue
+                </Button>
 
-                {otp.map((data, index) => {
-                  return (
-                    <InputNumber
-                      className="otp-filed"
-                      maxLength={1}
-                      min={0}
-                      key={index}
-                      defaultValue={null}
-                      value={data}
-                      onChange={(e) => handleInputChange(e, index)}
-                      onFocus={() => handleInputFocus(index)}
-                      ref={(ref) => (inputRefs.current[index] = ref)}
-                    />
-                  )
-                })}
-              </Form.Item>
-              <Button
-                style={{
-                  width: "500px",
-                  height: "50px",
-                }}
-                className="submit btn"
-                htmlType="submit"
-              >
-                Continue
-              </Button>
-
-              <div className="login-forgot">I didn't receive an email</div>
-              <ToastContainer />
-            </Form>
+                {/* <div className="login-forgot">I didn't receive an email</div> */}
+                <ToastContainer />
+              </Form>
+            </div>
           </div>
         </div>
       </div>
